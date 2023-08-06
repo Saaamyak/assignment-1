5549c39ce0d329bbe4abcc4dc0920a9c682b3f79
Saaamyak <samyakgoel5@gmail.com>
2023-08-06 13:34:13 +0530

    first commit

---
+++ src/component/list/List.jsx
@@ -0,0 +1,42 @@
+import ListRow from "./ListRow";
+import ListRowCell from "./ListRowCell";
+
+import ListHeader from "./ListHeader";
+import ListHeaderCell from "./ListHeaderCell";
+
+import styles from "./List.module.css";
+
+const List = ({ rows, setSelectedOrderDetails, setSelectedOrderTimeStamps, currency = "USD" }) => {
+  return (
+    <table className={styles.container}>
+      <thead>
+        <ListHeader>
+          <ListHeaderCell>Order ID</ListHeaderCell>
+          <ListHeaderCell>Buy/Sell</ListHeaderCell>
+          <ListHeaderCell>Country</ListHeaderCell>
+          <ListHeaderCell>Order Submitted</ListHeaderCell>
+          <ListHeaderCell>Order Volume / USD</ListHeaderCell>
+        </ListHeader>
+      </thead>
+      <tbody>
+        {rows.map((row, index) => (
+          <ListRow 
+            key={`list-item-${index}`} 
+            onClick={() => {
+              setSelectedOrderDetails(row?.executionDetails);
+              setSelectedOrderTimeStamps(row?.timestamps);
+            }}
+          >
+            <ListRowCell>{row["&id"]}</ListRowCell>
+            <ListRowCell>{row.executionDetails.buySellIndicator}</ListRowCell>
+            <ListRowCell>{row.executionDetails.orderStatus}</ListRowCell>
+            <ListRowCell>{row?.timestamps?.orderSubmitted}</ListRowCell>
+            <ListRowCell>{row.bestExecutionData.orderVolume[currency]}</ListRowCell>
+          </ListRow>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default List;
